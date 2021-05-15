27863716ecd69ad45ceec9892dcd5479c6c90912
strixsoul <rsilveira136@gmail.com>
2021-05-15 18:10:08 -0300

    added multer to the project

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,28 +1,26 @@
-import { request, response, Router } from 'express';
+import { Router } from 'express';
+import multer from 'multer';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: 'Category already exists!' });
-  }
-
-  categoriesRepository.create({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  const all = categoriesRepository.list();
+  return listCategoriesController.handle(request, response);
+});
 
-  return response.json(all);
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
