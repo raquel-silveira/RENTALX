da70a034cbd255fef59016ea9fcfe195907d0e68
strixsoul <rsilveira136@gmail.com>
2021-07-31 20:37:03 -0300

    created authenticate user and create category tests

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,43 @@
+import { CategoriesRepositoryInMemory } from '@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
+
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it('should be able to create a new category', async () => {
+    await createCategoryUseCase.execute({
+      name: 'Category Test',
+      description: 'Category description test',
+    });
+
+    const category = await categoriesRepositoryInMemory.findByName(
+      'Category Test'
+    );
+
+    expect(category).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category if category already exists', async () => {
+    expect(async () => {
+      await createCategoryUseCase.execute({
+        name: 'Category Test',
+        description: 'Category description test',
+      });
+
+      await createCategoryUseCase.execute({
+        name: 'Category Test',
+        description: 'Category description test',
+      });
+    }).rejects.toEqual(new AppError('Category already exists!'));
+  });
+});
