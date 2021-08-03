084aeb92c256fa5c7bf8810786d7241179461c44
strixsoul <rsilveira136@gmail.com>
2021-08-03 20:16:10 -0300

    created list available cars use case and repository

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from 'tsyringe';
+
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+interface IRequest {
+  brand?: string;
+  category_id?: string;
+  name?: string;
+}
+
+@injectable()
+class ListAvailableCarsUseCase {
+  constructor(
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
+  ) {}
+  async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
+    const allCars = await this.carsRepository.findAvailable(
+      brand,
+      category_id,
+      name
+    );
+    return allCars;
+  }
+}
+
+export { ListAvailableCarsUseCase };
