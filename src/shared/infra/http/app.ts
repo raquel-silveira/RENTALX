e68250ab32e3b4715486180ab3ca8f9652831679
strixsoul <rsilveira136@gmail.com>
2021-08-21 16:27:49 -0300

    created providers to upload image on local storage or amazon s3 storage

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,12 +1,16 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
+import 'reflect-metadata';
 
-import { AppError } from '@shared/errors/AppError';
-import createConnection from '@shared/infra/typeorm';
+import 'dotenv/config';
 
 import '@shared/container';
+
+import express, { NextFunction, Request, Response } from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import upload from '@config/upload';
+import { AppError } from '@shared/errors/AppError';
+import createConnection from '@shared/infra/typeorm';
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
@@ -18,6 +22,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
